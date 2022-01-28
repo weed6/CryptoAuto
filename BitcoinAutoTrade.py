258d9b31f94612a69ad56b459a05b84658ae1e64
JongSeokPark <pjs1567@gmail.com>
2022-01-29 00:33:34 +0900

    commit

--- BitcoinAutoTrade.py
+++ BitcoinAutoTrade.py
@@ -1,9 +1,24 @@
 import time
 import pyupbit
 import datetime
+import numpy as np
 
 access = "IPs6coOa3zg7ZgVdMdXZb4RnDbyrnFxr4vLTGr66"          # 본인 값으로 변경
-secret = "wdaYlczI3x12bLeL2xAAcFktwYslCiJYxkBhgN88"          # 본인 값으로 변경a
+secret = "wdaYlczI3x12bLeL2xAAcFktwYslCiJYxkBhgN88"          # 본인 값으로 변경
+coin = "SAND" # 원화마켓 코인
+
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-"+coin, count=7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -40,20 +55,28 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-"+coin)
         end_time = start_time + datetime.timedelta(days=1)
 
+        bestK = 0.5
+        bestRor = 0
+        for k in np.arange(0.1, 1.0, 0.1):
+            ror = get_ror(k)
+            if ror > bestRor:
+                bestRor = ror
+                bestK = k
+
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price("KRW-"+coin, bestK)
+            current_price = get_current_price("KRW-"+coin)
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-"+coin, krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            coinBal = get_balance(coin)
+            if coinBal > 5100/pyupbit.get_current_price("KRW-"+coin):
+                upbit.sell_market_order("KRW-"+coin, coinBal*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
--- test.py
+++ test.py
@@ -7,3 +7,4 @@ upbit = pyupbit.Upbit(access, secret)
 print(upbit.get_balance("KRW"))     # 현금
 print(upbit.get_balance("SAND"))    # 코인 갯수
 print(upbit.get_avg_buy_price("SAND"))
+print(5000/pyupbit.get_current_price("KRW-BTC"))
