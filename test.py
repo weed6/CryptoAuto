bc01d63dc8a754175d5c901b9172329cad56d1ae
JongSeokPark <pjs1567@gmail.com>
2022-01-28 23:30:13 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import pyupbit
+
+access = "IPs6coOa3zg7ZgVdMdXZb4RnDbyrnFxr4vLTGr66"          # 본인 값으로 변경
+secret = "wdaYlczI3x12bLeL2xAAcFktwYslCiJYxkBhgN88"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+print(upbit.get_balance("KRW"))     # 현금
+print(upbit.get_balance("SAND"))    # 코인 갯수
+print(upbit.get_avg_buy_price("SAND"))
